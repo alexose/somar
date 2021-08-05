fa7630417f0cc1a542d4183e56d56d1cde38c0c7
Alexander Ose <alexander@parsely.com>
2021-08-04 21:47:23 -0700

    Adding more artists

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ const exhibits = [
                 "At SOMAR I worked on my second childrens book, a story about a Sprite (fairy-human), named Ryuka that keeps a pet cemetery until the cemetery itself dies and Ryuka is left to create an entirely new universe. Alex and Maluhia were incredible hosts and the inspiring and restorative setting at SOMAR invited us into a relaxed and focused artistic process. I felt so fulfilled by everything I walked away with, from starting my project and making great progress on it, connecting with my artistic process, finding moments of connection to myself and nature, and feeling the gratitude of generous hospitality from the hosts."
             </p>
         `,
-        url: ''
+        url: 'odz8cbt2xp'
     },
     {
         name: 'Ken Kinoshita',
@@ -59,18 +59,37 @@ const exhibits = [
                 "My work consisted on a series of studies that capture the true color that defines the local, changing landscape. Challenging the post industrial bias that assumes color as an infinitely repeatable and consistent medium, I instead captured one color by layering different hues while being mindful how each dominated and blended with each other. The resulting aggregate recreated my observations that in nature there is no such thing as a uniform surface or color. Three sets of color studies culminated in one semi-figurative landscape drawing that applies the field observations into an image.'
             </p>
         `,
-        url: ''
+        url: 'hffmpwu2i0'
+    },
+    {
+        name: 'Yasaman Sheri',
+        form: 'AI Botany',
+        statement: `
+            <p>
+                Yasaman explored generative botany with an AI trained on the native species around the property.
+            </p>
+        `,
+    },
+    {
+        name: 'Sylvia Tan',
+        form: 'Ghanian Textiles',
+        statement: `
+            <p>
+                "The Kente Collaborative is a consortium of Ghana-based artisans and a DC-based designer.  Vintage, hand-woven West African textiles (e.g., Ashanti and Ewe kente and Yoruba aso oke) are reworked into the creation of personal accessories that are at once functional and beautiful."
+            </p>
+        `,
+    },
+    {
+        name: 'Denise Wang',
+        form: 'Origami',
+        statement: `
+            <p>
+
+            </p>
+        `,
     },
 ];
 
-/*
-{
-    name: 'Yasaman Sheri',
-    form: 'AI Botany',
-    statement: 'neat',
-    url: '5gct71l8hm'
-},
-*/
 
 // Exhibit functionality
 function loadExhibits() {
@@ -94,8 +113,11 @@ function loadExhibits() {
                     <h2>${name} - ${form}</h2>
                     <p>${statement}</p>
                 </div>
-                <iframe src="https://photodump-app-wmp3ybs6ua-uw.a.run.app/#${url}!light!silent!horizontal" class="photos"></iframe>
             `;
+
+            if (url) {
+                container.innerHTML += `<iframe src="https://photodump-app-wmp3ybs6ua-uw.a.run.app/#${url}!light!silent!horizontal" class="photos"></iframe>`
+            }
 
             window.goBack = function(e){
                 // TODO: update url
